584cb1b0458266ca28a5a97e0375d0b1962decc3
abarillas <abarillas@nbwaudev04>
2019-05-08 15:27:35 -0600

    Added service and formatter classes.

---
+++ integration-test/src/main/java/gt/edu/umg/ingenieria/sistemas/is/integration/test/CalculatorService.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.is.integration.test;
+
+public class CalculatorService {
+
+    public String doAdd(int a, int b) {        
+        return String.valueOf(a + b);        
+    }
+    
+    public String doSub(int a, int b) {
+        return String.valueOf(a - b);
+    }
+    
+}
---
+++ integration-test/src/main/java/gt/edu/umg/ingenieria/sistemas/is/integration/test/ResultFormatter.java
@@ -0,0 +1,9 @@
+package gt.edu.umg.ingenieria.sistemas.is.integration.test;
+
+public class ResultFormatter {
+
+    public int format(String result) throws NumberFormatException {        
+        return Integer.parseInt(result);        
+    }
+    
+}
